39032b24b0432e1ee5e0a5beb28e0549a1e2117c
Amirreza <155296711+Amirrezach1383@users.noreply.github.com>
2025-01-22 15:52:13 +0330

    change the size and put frame of picture

--- SocialNetwork/main.cpp
+++ SocialNetwork/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-
+#include "login_signup.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    // w.show();
+    Login_SignUp s;
+    s.show();
     return a.exec();
 }
