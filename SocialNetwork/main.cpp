730d13cc42cb0a2cef99f96d279baa4c89df1558
Amirreza <155296711+Amirrezach1383@users.noreply.github.com>
2025-01-26 18:49:49 +0330

    finished designing request dialog window the backgrounds set after all designing finished

--- SocialNetwork/main.cpp
+++ SocialNetwork/main.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 #include "login_signup.h"
 #include <QApplication>
+#include <requests.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    Requests r;
+    r.show();
+
     // w.show();
     Login_SignUp s;
-    s.show();
+    // s.show();
     return a.exec();
 }
