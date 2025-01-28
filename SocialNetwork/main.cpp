18ceabdc5020679238b62bab96ac390c66ae685a
Amirreza <155296711+Amirrezach1383@users.noreply.github.com>
2025-01-28 21:22:48 +0330

    finished merge the design_suggestWidget branch

--- SocialNetwork/main.cpp
+++ SocialNetwork/main.cpp
@@ -1,8 +1,6 @@
 #include "login_signup.h"
 #include <QApplication>
 #include <database.h>
-#include <requests.h>
-#include <suggestwidget.h>
 
 int main(int argc, char *argv[]) {
 
@@ -11,14 +9,6 @@ int main(int argc, char *argv[]) {
     DataBase database;
     Login_SignUp login_Signup(&database);
     login_Signup.show();
-    //
-    User user;
-    QPixmap pic;
-    pic.load(":/Backgrounds/Premium_Vector_Woodland_forest_landscape_with_trees_enhanced.png");
-    user.setUsername("amir");
-    user.setAvatar(pic);
-    suggestWidget s(&user);
-    //
 
     return a.exec();
 }
